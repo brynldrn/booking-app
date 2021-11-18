6e32a582e8a01fd8ef3e0486c00635b0bd6ffd59
Bryan Aldrin Quinalayo <bryan.aldrin.quinalayo@codeandtheory.com>
2021-11-18 18:29:11 +0800

    ✨ Initial working setup with MUI

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
+import { ThemeProvider, createTheme } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
